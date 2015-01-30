39ddc62f670638ba7c6ebd0cce7edd8ad13d6359
Tomasz Strejczek <tomasz.strejczek@mbank.pl>
2015-01-30 23:00:10 +0100

    Tests fixed after adding predefined categories

--- app/src/androidTest/java/com/evenucleus/evenucleus/CategoryRepoT.java
+++ app/src/androidTest/java/com/evenucleus/evenucleus/CategoryRepoT.java
@@ -18,7 +18,7 @@ public class CategoryRepoT extends TestBase {
         categoryRepo._localdb = _localdb;
         categoryRepo._stringProvider = new StringProvider(getContext());
         List<String> list = categoryRepo.Get();
-        Assert.assertEquals(0, list.size());
+        Assert.assertEquals(1, list.size());   // predefined ones
     }
 
     public void test_AddCategory() throws SQLException, UserException {
@@ -28,8 +28,13 @@ public class CategoryRepoT extends TestBase {
         categoryRepo.AddCategory("ala");
 
         List<String> list = categoryRepo.Get();
-        Assert.assertEquals(1, list.size());
-        Assert.assertEquals("ala", list.iterator().next());
+        Assert.assertEquals(2, list.size()); // additional one predefined
+        boolean found = false;
+        for(String s: list)
+            if (s.equals("ala"))
+                found = true;
+
+        Assert.assertTrue(found);
     }
 
     public void test_AddCDuplicateategory() throws SQLException, UserException {
